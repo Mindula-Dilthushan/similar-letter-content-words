c29e71a2146691b3399bf9c9274d4f43509d71ec
Mindula-Dilthushan <minduladilthushan1@gmail.com>
2022-05-07 13:09:59 +0530

    update

--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 from itertools import permutations
-data = [''.join(p) for p in permutations(['s', 't', 'a', 'c', 'k'])]
-print(data)
+
+with open('data/simplewords.txt', 'r') as file:
+    for permutations in file:
+        permutations = permutations.strip()
+
+        def permu(data):
+            return data == data[::-1]
+
+        display = permu(permutations.strip())
+
+        if display:
+            print("this is permutations")
+        else:
+            print("this is not permutations")
